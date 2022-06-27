2c095c24be6a2b264e89766feef80047b90efde6
Trashmosphere <91850662+Trashmosphere@users.noreply.github.com>
2022-06-27 13:55:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ streamlit.title("My Parents New Healty Diner")
 
 streamlit.header("Breakfast Menu")
 
-streamlit.text("Omega3 Blueberry OatMeal")
-streamlit.text("Kale, Spinach and rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.text("🥣 Omega3 Blueberry OatMeal")
+streamlit.text("🥗 Kale, Spinach and rocket Smoothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
