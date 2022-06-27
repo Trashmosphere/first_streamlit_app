de9f0f4d0c2527b981eca79a7bed6ee77963fc47
Trashmosphere <91850662+Trashmosphere@users.noreply.github.com>
2022-06-27 13:53:55 +0300

    Update streamlit_app.py
    + header and text strings

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title("My Parents New Healty Diner")
+
+streamlit.header("Breakfast Menu")
+
+streamlit.text("Omega3 Blueberry OatMeal")
+streamlit.text("Kale, Spinach and rocket Smoothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
